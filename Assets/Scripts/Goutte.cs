fdca0eacec9b4f17111a4e620a1aeaa80414805d
Patricio <jpdibacco@gmail.com>
2020-11-24 22:46:01 +0100

    moved scripts to scripts folder

---
+++ Assets/Scripts/Goutte.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goutte : MonoBehaviour{
+    public Vector3 velocity;
+    public int force;
+    private float cptGoutte;
+    void Start(){
+        GetComponent<Rigidbody>().AddForce(velocity*force);
+    }
+    void OnCollisionEnter(Collision collision){
+        // this didn't work ....Destroy(this);
+        Destroy(gameObject);
+    }
+    // void Update() {
+    //     cptGoutte -= Time.deltaTime;
+    //     if(cptGoutte <= 0f){
+    //         GameObject goutte = Instantiate (gouttePrefab, transform.position + groutteOffset, Quaternion.identity) as GameObject;
+    //         goutte.GetComponent <ScriptGoutte().velocity = new Vector3 (-mouvement.x, speed, 0f);
+    //     }
+    // }
+
+
+}
---
+++ Assets/Scripts/MeatBoy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class MeatBoy : MonoBehaviour{
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+    private Vector3 mouvement;
+    private CharacterController controller;
+    private int jumpsCount = 0;
+    public int jumpsMax;
+    public GameObject gouttePrefab;
+    public float delayGoutte;
+    private float cptGoutte;
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();    
+    }
+    void Update() {
+        mouvement.x = Input.GetAxisRaw("Horizontal");
+        mouvement.y -= gravity * Time.deltaTime;
+        if(controller.isGrounded){
+            mouvement.y = 0;
+            jumpsCount = 0;
+        }
+        // this didn't work: if(Input.GetButtonDown("Jump")){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            mouvement.y = jumpSpeed;
+            if(jumpsCount < jumpsMax){
+                jumpsCount++;
+            }
+            Debug.Log(jumpsCount);
+        }
+        controller.Move(mouvement * Time.deltaTime * speed);
+        // Création des gouttes quand MeatBoy court:
+        if(mouvement.x != 0f){
+            cptGoutte -= Time.deltaTime;
+            if(cptGoutte<=0){
+                delayGoutte = 0;
+            }
+        }
+    }
+
+
+
+}
